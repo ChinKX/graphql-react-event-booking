f78557dfc7ca6a1a8051bb8277fc8539424faf30
Chin Kai Xiang <chinkx1205@gmail.com>
2020-07-22 14:46:30 +0800

    Added graphql types

--- app.js
+++ app.js
@@ -13,17 +13,33 @@ const { graphqlHTTP  } = require('express-graphql');
 const { buildSchema } = require('graphql');
 
 const app = express();
+const events = [];
 
 app.use(bodyParser.json());
 
 app.use('/graphql', graphqlHTTP({
   schema: buildSchema(`
+    type Event {
+      _id: ID!
+      title: String!
+      description: String!
+      price: Float!
+      date: String!
+    }
+
+    input EventInput {
+      title: String!
+      description: String!
+      price: Float!
+      date: String!
+    }
+
     type RootQuery {
-      events: [String!]!
+      events: [Event!]!
     }
 
     type RootMutation {
-      createEvent(name: String): String
+      createEvent(eventInput: EventInput): Event
     }
 
     schema {
@@ -33,15 +49,18 @@ app.use('/graphql', graphqlHTTP({
   `),// define supported queries (graphql schema object),
   rootValue: {
     events: () => {
-      return [
-        'Romantic Cooking',
-        'Sailing',
-        'All-Night Coding'
-      ];
+      return events;
     },
     createEvent: (args) => {
-      const eventName = args.name;
-      return eventName;
+      const event = {
+        _id: Math.random().toString(),
+        title: args.eventInput.title,
+        description: args.eventInput.description,
+        price: +args.eventInput.price,
+        date: args.eventInput.date
+      };
+      events.push(event);
+      return event;
     }
   },// define the logic to process a particular query (bundle of all resolvers)
   graphiql: true
