a6a186a0bc76143f10bb5d050516bbb82260698f
Chin Kai Xiang <chinkx1205@gmail.com>
2020-07-24 12:41:17 +0800

    Setup navigation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,25 @@
 import React from 'react';
 import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
-// import './App.css';
+import './App.css';
 
 import AuthPage from './pages/Auth';
 import BookingsPage from './pages/Bookings';
 import EventsPage from './pages/Events';
+import MainNavigation from './components/Navigation/MainNavigation';
 
 function App() {
   return (
     <BrowserRouter>
-      <Switch>
-        {/* Use exact to avoid infinite redirection loop i.e. only redirect when the exact path matches */}
-        <Redirect from='/' to='/auth' exact />
-        <Route path='/auth' component={AuthPage} />
-        <Route path='/events' component={EventsPage} />
-        <Route path='/bookings' component={BookingsPage} />
-      </Switch>
+      <MainNavigation />
+      <main className='main-content'>
+        <Switch>
+          {/* Use exact to avoid infinite redirection loop i.e. only redirect when the exact path matches */}
+          <Redirect from='/' to='/auth' exact />
+          <Route path='/auth' component={AuthPage} />
+          <Route path='/events' component={EventsPage} />
+          <Route path='/bookings' component={BookingsPage} />
+        </Switch>
+      </main>
     </BrowserRouter>
   );
 }
